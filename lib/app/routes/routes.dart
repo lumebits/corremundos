cafba87a208805ad7021c0da20cf1e21956913c6
Paloma Piot <paloma.piot@udc.es>
2022-01-24 17:34:22 +0100

    add calendar view mock

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:corremundos/app/bloc/app_bloc.dart';
+import 'package:corremundos/calendar/view/calendar_page.dart';
 import 'package:corremundos/common/widgets/navigation.dart';
 import 'package:corremundos/create_trip/create_trip.dart';
 import 'package:corremundos/current_trip/view/current_trip_page.dart';
@@ -16,7 +17,7 @@ List<Page> onGenerateAppViewPages(AppState state, List<Page<dynamic>> pages) {
         case AppTab.current:
           return [CurrentTripPage.page()];
         case AppTab.calendar:
-          return [TripsPage.page()];
+          return [CalendarPage.page()];
         case AppTab.profile:
           return [ProfileDetailPage.page()];
         case AppTab.addTrip:
